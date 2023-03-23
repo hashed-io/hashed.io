6eab0e583b68401f8ab80e834ba685de479fb2d7
DaniPoot <DaniPoot@users.noreply.github.com>
2023-03-23 22:30:41 +0000

    deploy: 7762af699bb5bd8e55a8bd7a2bc0a0bee02fbb28

--- js/mailto.js
+++ js/mailto.js
@@ -1,12 +1,11 @@
 $form = document.querySelector('form')
 $form.addEventListener('submit', (event) => {
   event.preventDefault()
-  const url = event.srcElement.action
   const cc = document.querySelector('#email').value
   const subject = 'New Project'
   const body = document.querySelector('#message').value
 
-  const uri = `${url}?cc=${cc}&subject=${subject}&body=${body}`;
+  const uri = `mailto:max@hashed.io?cc=${cc}&subject=${subject}&body=${body}`;
   console.log(uri)
   window.open(uri);
 })
